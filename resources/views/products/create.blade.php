bf590943156aa5d809f408e8f6942e1352c1b49f
kitaDEV-stack <125789326+kitaDEV-stack@users.noreply.github.com>
2023-05-12 19:21:42 +0630

    simplecrud

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,65 @@
+@extends('layout.app')
+@section('content')
+    <main class="container">
+        <section>
+            <form method="post" action="{{ route('products.store') }}" enctype="multipart/form-data">
+                @csrf
+                <div class="titlebar">
+                    <h1>Add Product</h1>
+                </div>
+                @if ($errors->any)
+                    <div>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="card">
+                    <div>
+                        <label>Name</label>
+                        <input type="text" name="name">
+                        <label>Description (optional)</label>
+                        <textarea cols="10" rows="5" name="description"></textarea>
+                        <label>Add Image</label>
+                        <img src="" alt="" class="img-product" id="file-preview"/>
+                        <input type="file" name="image" accept="image/*" onchange="showFile(event)">
+                    </div>
+                    <div>
+                        <label>Category</label>
+                        <select name="category" id="">
+                            @foreach (json_decode('{"SmartPhone":"Smartphone","Smart Tv":"Smart Tv","Computer":"Computer"}',true) as $optionKey => $optionValue)
+
+                            <option value="{{ $optionKey }}">{{ $optionValue }}</option>
+                            @endforeach
+                        </select>
+                        <hr>
+                        <label>Inventory</label>
+                        <input type="text" name="quantity">
+                        <hr>
+                        <label>Price</label>
+                        <input type="text" name="price">
+                    </div>
+                </div>
+                <div class="titlebar">
+                    <h1></h1>
+                    <button>Save</button>
+                </div>
+            </form>
+        </section>
+    </main>
+    <script>
+        function showFile(event){
+            var input = event.target;
+            var reader = new FileReader();
+            reader.onload = function(){
+                var dataURL = reader.result;
+                var output = document.getElementById('file-preview');
+                output.src = dataURL;
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    </script>
+@endsection
